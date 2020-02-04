085206b1f8437a5d1b85ccaf3cd4e2cb462de029
danilo100kl <danilo100kl@hotmail.com>
2020-02-04 18:45:54 -0300

    Atualização  variáveis cena

--- apartamento/_cena.py
+++ apartamento/_cena.py
@@ -62,8 +62,6 @@ class Cena():
         pia_geladeira = OBJ('pia_geladeira.obj')
         tapete_1 = OBJ('tapete_1.obj')
         tomadas = OBJ('tomadas.obj')
-        # tomada_1 = OBJ('tomada_1.obj')
-        # tomada_2 = OBJ('tomada_2.obj')
 
         tapete_2 = OBJ('tapete_2.obj')
         rack_1_TV = OBJ('rack_1_TV.obj')
@@ -78,14 +76,14 @@ class Cena():
         banheira = OBJ('banheira.obj')
         rack_3 = OBJ('rack_3.obj')
 
-        colisoes = [fogao, pia_geladeira, rack_1_TV, sofa] # acabamento_1, acabamento_2, sofa
+        colisoes_sala_cozinha = [fogao, pia_geladeira, rack_1_TV, sofa] # acabamento_1, acabamento_2, sofa
         for i in range(len(paredes)):
-            colisoes.append(paredes[i])
+            colisoes_sala_cozinha.append(paredes[i])
 
         glMatrixMode(GL_PROJECTION)
         glLoadIdentity()
 
-        #mouse
+        #mouse camera
         width, height = viewport
         gluPerspective(70.0, width/float(height), 1, 100.0)
         glEnable(GL_DEPTH_TEST)
@@ -96,22 +94,21 @@ class Cena():
 
         rotacao_tomme = 0
         rotacao_mouse = False
-        move = False
+        mover_camera = False
 
-        move_forward = False
-        move_back    = False
-        move_left    = False
-        move_right   = False
-        move_speed   = 0.2
+        mover_para_frente = False
+        mover_para_tras    = False
+        mover_para_esquerda    = False
+        mover_para_direita   = False
+        mover_tomme   = 0.2
 
-        open_speed = 6
         abrir_porta_2 = False
         abrir_porta_3 = False
 
         luz_ligada = False
         
-        aux_colisoes1 = [paredes[8], paredes[9], paredes[10], cama, guarda_roupa, rack_2, OBJ('porta_aberta_2.obj')]
-        aux_colisoes2 = [paredes[3], paredes[4], paredes[5], vaso_sanitario, pia_banheiro, banheira, rack_3, OBJ('porta_aberta_3.obj')]
+        colisoes_quarto = [paredes[8], paredes[9], paredes[10], cama, guarda_roupa, rack_2, OBJ('porta_aberta_2.obj')]
+        colisoes_banheiro = [paredes[3], paredes[4], paredes[5], vaso_sanitario, pia_banheiro, banheira, rack_3, OBJ('porta_aberta_3.obj')]
 
         while True:
             tempo.tick(30)
@@ -178,25 +175,13 @@ class Cena():
                     pygame.quit()
                 elif e.type == KEYDOWN:
                     if e.key == K_w:
-                        move_forward = True
+                        mover_para_frente = True
                     elif e.key == K_s:
-                        move_back    = True
+                        mover_para_tras    = True
                     elif e.key == K_a:
-                        move_left    = True
+                        mover_para_esquerda    = True
                     elif e.key == K_d:
-                        move_right   = True
-                    elif e.key == K_RIGHT:
-                        tomme
-                        glRotate(90, 0, 1, 0)
-                        rotacao_tomme += 90
-                        if rotacao_tomme == 360:
-                            rotacao_tomme = 0
-                    elif e.key == K_LEFT:
-                        tomme
-                        glRotate(-90, 0, 1, 0)
-                        rotacao_tomme -= 90
-                        if rotacao_tomme == -360:
-                            rotacao_tomme = 0  
+                        mover_para_direita   = True  
                     if e.key == K_q:
                         if luz_ligada:
                             luz_ligada = False
@@ -206,13 +191,13 @@ class Cena():
                         sys.exit()               
                 elif e.type == KEYUP:
                     if e.key == K_w:
-                        move_forward = False
+                        mover_para_frente = False
                     elif e.key == K_s:
-                        move_back    = False
+                        mover_para_tras    = False
                     elif e.key == K_a:
-                        move_left    = False
+                        mover_para_esquerda    = False
                     elif e.key == K_d:
-                        move_right   = False
+                        mover_para_direita   = False
 
                 elif e.type == MOUSEBUTTONDOWN:
                     if e.button == 4:
@@ -222,27 +207,27 @@ class Cena():
                     elif e.button == 1:
                         rotacao_mouse = True
                     elif e.button == 3:
-                        move = True
+                        mover_camera = True
                 elif e.type == MOUSEBUTTONUP:
                     if e.button == 1:
                         rotacao_mouse = False
                     elif e.button == 3:
-                        move = False
+                        mover_camera = False
                 elif e.type == MOUSEMOTION:
                     i, j = e.rel
                     if rotacao_mouse:
                         rx += i
                         ry += j
-                    if move:
+                    if mover_camera:
                         tx += i
                         ty -= j
 
             if(abrir_porta_2):
-                colisao_tomme = chek_collisions(tomme, aux_colisoes1)
+                colisao_tomme = chek_collisions(tomme, colisoes_quarto)
             elif(abrir_porta_3):
-                colisao_tomme = chek_collisions(tomme, aux_colisoes2)
+                colisao_tomme = chek_collisions(tomme, colisoes_banheiro)
             else:
-                colisao_tomme = chek_collisions(tomme, colisoes)
+                colisao_tomme = chek_collisions(tomme, colisoes_sala_cozinha)
             colisao_porta_2 = chek_collisions(tomme,[porta_fora_2, porta_dentro_2])
             colisao_porta_3 = chek_collisions(tomme,[porta_fora_3, porta_dentro_3])
 
@@ -258,67 +243,22 @@ class Cena():
             else:
                 abrir_porta_3 = False
 
-            # if colisao_porta_3['right'] != 0 or colisao_porta_3['left'] != 0 or colisao_porta_3['up'] != 0 or colisao_porta_3['down'] != 0:
-            #     abrir_porta_3 = True
-            # else:
-            #     abrir_porta_3 = False
-
-            if move_forward:
-                posicao = list(tomme.pos)
-                if rotacao_tomme == 0 and colisao_tomme['up'] == 0:          
-                    posicao[0] -= move_speed
-                    tomme.pos = tuple(posicao)
-                elif ((rotacao_tomme == 90 or rotacao_tomme == -270) and colisao_tomme['right'] == 0):
-                    posicao[2] -= move_speed
-                    tomme.pos = tuple(posicao)
-                elif ((rotacao_tomme == 180 or rotacao_tomme == -180) and colisao_tomme['down'] == 0):
-                    posicao[0] += move_speed
-                    tomme.pos = tuple(posicao)
-                elif ((rotacao_tomme == 270 or rotacao_tomme == -90) and colisao_tomme['left'] == 0):
-                    posicao[2] += move_speed
-                    tomme.pos = tuple(posicao)
-            elif move_back: 
+            if (mover_para_frente and colisao_tomme['up'] == 0):
+                posicao = list(tomme.pos)       
+                posicao[0] -= mover_tomme
+                tomme.pos = tuple(posicao)
+            elif (mover_para_tras and colisao_tomme['down'] == 0): 
                 posicao = list(tomme.pos)    
-                if (rotacao_tomme == 0 and colisao_tomme['down'] == 0):
-                    posicao[0] += move_speed
-                    tomme.pos = tuple(posicao)
-                elif ((rotacao_tomme == 90 or rotacao_tomme == -270) and colisao_tomme['left'] == 0):
-                    posicao[2] += move_speed
-                    tomme.pos = tuple(posicao)
-                elif ((rotacao_tomme == 180 or rotacao_tomme == -180) and colisao_tomme['up'] == 0):
-                    posicao[0] -= move_speed
-                    tomme.pos = tuple(posicao)
-                elif ((rotacao_tomme == 270 or rotacao_tomme == -90) and colisao_tomme['right'] == 0):
-                    posicao[2] -= move_speed
-                    tomme.pos = tuple(posicao)
-            elif move_left:
+                posicao[0] += mover_tomme
+                tomme.pos = tuple(posicao)
+            elif (mover_para_esquerda and colisao_tomme['left'] == 0):
                 posicao = list(tomme.pos)    
-                if (rotacao_tomme == 0 and colisao_tomme['left'] == 0):
-                    posicao[2] += move_speed
-                    tomme.pos = tuple(posicao)
-                elif ((rotacao_tomme == 90 or rotacao_tomme == -270) and colisao_tomme['up'] == 0):
-                    posicao[0] -= move_speed
-                    tomme.pos = tuple(posicao)
-                elif ((rotacao_tomme == 180 or rotacao_tomme == -180) and colisao_tomme['right'] == 0):
-                    posicao[2] -= move_speed
-                    tomme.pos = tuple(posicao)
-                elif ((rotacao_tomme == 270 or rotacao_tomme == -90) and colisao_tomme['down'] == 0):
-                    posicao[0] += move_speed
-                    tomme.pos = tuple(posicao)
-            elif move_right:
+                posicao[2] += mover_tomme
+                tomme.pos = tuple(posicao)
+            elif (mover_para_direita and colisao_tomme['right'] == 0):
                 posicao = list(tomme.pos)
-                if (rotacao_tomme == 0 and colisao_tomme['right'] == 0):
-                    posicao[2] -= move_speed
-                    tomme.pos = tuple(posicao)
-                elif ((rotacao_tomme == 90 or rotacao_tomme == -270) and colisao_tomme['down'] == 0):
-                    posicao[0] += move_speed
-                    tomme.pos = tuple(posicao)
-                elif ((rotacao_tomme == 180 or rotacao_tomme == -180) and colisao_tomme['left'] == 0):
-                    posicao[2] += move_speed
-                    tomme.pos = tuple(posicao)
-                elif ((rotacao_tomme == 270 or rotacao_tomme == -90) and colisao_tomme['up'] == 0):
-                    posicao[0] -= move_speed
-                    tomme.pos = tuple(posicao)
+                posicao[2] -= mover_tomme
+                tomme.pos = tuple(posicao)
 
             glPopMatrix()
             pygame.display.flip()
